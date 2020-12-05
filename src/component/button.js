9eb7f55e7e64851525a6952c6fce7d72a0e0b5b2
KONG41 <sokkongchan@gmail.com>
2020-12-05 12:46:07 +0700

    second exit

--- src/component/button.js
+++ src/component/button.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Form, Group, Label, Control, Row, Col, Button } from "react-bootstrap";
 const Btn = (props) => {
   return (
-    <Button variant="primary" className="btn-sub">
+    <Button variant="primary" className={props.className}>
       {props.value}
     </Button>
   );
--- src/component/login.js
+++ src/component/login.js
@@ -1,23 +1,43 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Form, Group, Label, Control, Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import InputFile from "./input";
+import Btn from "./button";
+import InputFile from "./inputFile";
+import Header from "./header";
 const Login = () => {
   return (
-    <div className="container">
-      <label className="login">LOGIN</label>
-      <div>
-        <InputFile name="Name" />
-        <InputFile name="Password" />
-      </div>
-      <div className="btn">
-        <label>Login</label>
-      </div>
-      <Link to="/Singup">
-        <div className="btn1">
-          <label>Sing Up</label>
+    <div className="singup-con">
+      <div className="singup-form">
+        <Header name="Login" detail="Please fill this to login your account" />
+        <Form className="fill-form">
+          <Form.Group controlId="formGroupUsername">
+            <InputFile type="email" name="Email" placeholder="username@email" />
+          </Form.Group>
+          <Form.Group controlId="formGroupPassword">
+            <InputFile type="password" name="Password" placeholder="password" />
+          </Form.Group>
+        </Form>
+        <div className="detail">
+          <li>
+            <a
+              href="https://web.facebook.com/login/identify?_rdc=1&_rdr"
+              target="_blank"
+            >
+              Forgot Password.
+            </a>
+          </li>
         </div>
-      </Link>
+        <div className="detail">
+          <p>Click singup if you don't have account.</p>
+        </div>
+        <Link to="/singup">
+          <Btn value="SingUp" className="btn-singup"></Btn>
+        </Link>
+        <Link to="/home">
+          <Btn value="Login" className="btn-login"></Btn>
+        </Link>
+      </div>
     </div>
   );
 };
